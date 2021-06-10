d6f4a9c209b0e02134d2214906f5c417dce0b29a
Thomas Michels Rodrigues <thomas.rodrigues@ambevtech.com.br>
2021-06-10 15:35:54 -0300

    Creating new column in orders

--- app/domain/models/orders.py
+++ app/domain/models/orders.py
@@ -8,6 +8,7 @@ from uuid import uuid4
 class Order(Base):
     __tablename__ = 'orders'
     id = Column(String, default=lambda: str(uuid4()), primary_key=True)
+    user_id = Column(String, ForeignKey('users.id'))
     sale_id = Column(String, ForeignKey('sales.id'))
     product_id = Column(String, ForeignKey('products.id'))
     quantity = Column(Integer, nullable=False)
@@ -15,12 +16,14 @@ class Order(Base):
 
     product = relationship('Product', uselist=False)
     sale = relationship('Sale', uselist=False)
+    user = relationship('User', uselist=False)
 
     def serialize(self) -> dict:
         return {
             'id': self.id,
             'sale': self.sale.serialize(),
             'product': self.product.serialize(),
+            'user': self.user.serialize(),
             'quantity': self.quantity,
             'active': self.active
         }
